055a931529b0d329f09bda9de10e00bd9d05366a
Israel Andreotti <israel.andreotti01@gmail.com>
2023-05-22 09:51:28 -0300

    scripts criados

--- createTable.sql
+++ createTable.sql
@@ -4,22 +4,22 @@ CREATE TABLE aluno (
     ra BIGINT NOT NULL,
     email VARCHAR(255) NOT NULL,
     fone VARCHAR(20) NOT NULL
-)
+);
 
 CREATE TABLE uc (
     carga_horaria BIGINT NOT NULL,
     nome VARCHAR(50) NOT NULL,
     id BIGINT NOT NULL,
     descricao VARCHAR(255),
-    ra_aluno PK,
-    id_professor PK 
-)
+    ra_aluno BIGINT NOT NULL,
+    id_professor BIGINT NOT NULL 
+);
 
 CREATE TABLE plano_curricular (
     id BIGINT NOT NULL,
     descricao VARCHAR(255),
-    id_uc FK
-)
+    id_uc BIGINT NOT NULL
+);
 
 CREATE TABLE professor (
     id BIGINT NOT NULL,
@@ -28,17 +28,17 @@ CREATE TABLE professor (
     fone VARCHAR(20),
     email VARCHAR(55),
     data_contratacao TIMESTAMP NOT NULL,
-)
+);
 
 CREATE TABLE turma (
     id BIGINT NOT NULL,
     turno VARCHAR(50),
     dia_aula VARCHAR(30),
-    id_uc PK,
-    id_salas PK,
-    id_professor PK,
-    id_aluno PK
-)
+    id_uc BIGINT NOT NULL,
+    id_sala BIGINT NOT NULL,
+    id_professor BIGINT NOT NULL,
+    id_aluno BIGINT NOT NULL
+);
 
 CREATE TABLE campus (
     id BIGINT NOT NULL,
@@ -48,33 +48,32 @@ CREATE TABLE campus (
 CREATE TABLE sala (
     id BIGINT NOT NULL,
     capacidade INT NOT NULL,
-    id_predios PK
-) 
+    id_predio BIGINT NOT NULL
+);
 
 CREATE TABLE predio (
     id BIGINT NOT NULL,
     numero_salas_aula INT NOT NULL,
-    id_campus PK
-)
+    id_campus BIGINT NOT NULL
+);
 
 CREATE TABLE matricula (
-    id_aluno PK,
-    id_turma PK,
+    id_aluno BIGINT NOT NULL,
+    id_turma BIGINT NOT NULL,
     data_efetuacao TIMESTAMP,
     data_cancelamento TIMESTAMP,
     situacao BOOLEAN
-)
+);
 
 CREATE TABLE conteudo_curricular (
     id BIGINT NOT NULL,
-    descricao VARCHAR(255)
-    
-)
+    descricao VARCHAR(255)   
+);
 
 CREATE TABLE plano_conteudo (
-    id_planoCurricular PK
-    id_conteudoCurricular PK
-)
+    id_planoCurricular BIGINT NOT NULL,
+    id_conteudoCurricular BIGINT NOT NULL
+);
 
 ALTER TABLE alunos ADD PRIMARY KEY(ra);
 
@@ -88,4 +87,23 @@ ALTER TABLE plano_curricular ADD FOREIGN KEY(id_uc) REFERENCES uc(id);
 
 ALTER TABLE professor ADD PRIMARY KEY(id);
 
-ALTER TABLE turma
+ALTER TABLE turma ADD PRIMARY KEY(id);
+ALTER TABLE turma ADD FOREIGN KEY(id_uc) REFERENCES uc(id);
+ALTER TABLE turma ADD FOREIGN KEY(id_sala) REFERENCES sala(id);
+ALTER TABLE turma ADD FOREIGN KEY(id_professor) REFERENCES professor(id);
+ALTER TABLE turma ADD FOREIGN KEY(id_aluno) REFERENCES aluno(id);
+
+ALTER TABLE campus ADD PRIMARY KEY(id);
+
+ALTER TABLE sala ADD PRIMARY KEY(id);
+ALTER TABLE sala ADD FOREIGN KEY(id_predio) REFERENCES predio(id);
+
+ALTER TABLE matricula ADD FOREIGN KEY (id_aluno) REFERENCES aluno(id);
+ALTER TABLE matricula ADD FOREIGN KEY (id_turma) REFERENCES turma(id);
+
+ALTER TABLE conteudo_curricular ADD PRIMARY KEY(id);
+
+ALTER TABLE plano_conteudo ADD FOREIGN KEY(id_planoCurricular) REFERENCES plano_curricular(id);
+ALTER TABLE plano_conteudo ADD FOREIGN KEY(id_conteudoCurricular) REFERENCES conteudo_curricular(id);
+
+
